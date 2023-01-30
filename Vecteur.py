efe209d1093743c590750aa4db28f5787219310f
samy1004 <122973045+samy1004@users.noreply.github.com>
2023-01-30 12:32:10 +0100

    Update Vecteur.py

--- Vecteur.py
+++ Vecteur.py
@@ -14,12 +14,14 @@ class Vecteur:
     def produit_scalaire(self,other):
         return ((self.x * other.x) +(self.y * other.y))
     def produit_vectoriel(self,other):
-        pX = self.x * other.y
-        pY = self.y * other.x
+       pX = (self.x * other.x)+(self.x * other.y)
+        pY = (self.y * other.x)+(self.y * other.y)
+        print(pX,pY)
         return Vecteur(pX,pY)
     def derivation_vecteur(self,time):
         dxdt = np.diff(self.x)/ np.mean(np.diff(time))
         dydt = np.diff(self.y)/ np.mean(np.diff(time))
+        print(dxdt,dxdt)
         return Vecteur(dxdt,dxdt)
     def vecteur_acceleration(self,time):
         vecteur_vitesse = self.derivation_vecteur(time)
