152128daa7c71829bd8e6b1f484d75be8084a799
Brainyranger <davidlumbwe@gmail.com>
2023-02-01 04:52:05 +0100

    validation

--- projet_robot/script/Graphic.py
+++ projet_robot/script/Graphic.py
@@ -2,13 +2,13 @@ import pygame
 import math
 
         
-class Graphic:
+class Graphics:
     def __init__(self,x,y)-> None:
-        self.robot = pygame.image.load("images.jpg") 
+        self.robot = pygame.image.load("/home/david/iROBOT-/projet_robot/script/images.jpg") 
         self.rect = self.robot.get_rect(x=x,y=y)
         self.speed = 5
         self.velocity = [0,0]
-        self.obstacle = pygame.image.load("style-realiste-bombe-noire-ronde.jpg")
+        self.obstacle = pygame.image.load("/home/david/iROBOT-/projet_robot/script/style-realiste-bombe-noire-ronde.jpg")
     
     def move(self):  
         self.rect.move_ip(self.velocity[0]*self.speed,self.velocity[1]*self.speed) 
@@ -17,7 +17,7 @@ class Graphic:
         screen.blit(self.robot,self.rect)
     
     def draw_obstacle(self,screen,x,y):
-        self.rect_obs = self.obstacle.get_rect((x,y))
+        self.rect_obs = self.obstacle.get_rect(x=x,y=y)
         screen.blit(self.obstacle,self.rect_obs)
         
 
