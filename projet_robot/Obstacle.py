a96b9897a851a739c6527653f0fc04e8d4d29c36
greggggggggggggggg <122972887+greggggggggggggggg@users.noreply.github.com>
2023-02-08 23:56:31 +0000

    Supression de pygame dans l'init de la classe Obstacle

--- projet_robot/Obstacle.py
+++ projet_robot/Obstacle.py
@@ -6,21 +6,22 @@ import numpy as np
 class Obstacle:
     #création d'obstacle
     
-    def __init__(self,x,y,nom):
+    def __init__(self,x,y,nom,taille_x,taille_y):
         """ enresgistre les coordoonnées de nos obstacles"""
         self.x=x
         self.y=y
         self.nom = nom
+        self.taille_x = taille_x
+        self.taille_y = taille_y
         self.list_obstacle = []
         self.list_obstacle.append([x,y])
         self.colour = (0,0,255)
-        self.obstacle = pygame.Rect(x,y,20,20)
     #affichage d'obstacle
     
     def draw_obstacle(self,screen):
         """ dessine nos obstacles selon ses coordonnées"""
-  
-        pygame.draw.rect(screen,self.colour,self.obstacle)
+        obstacle = pygame.Rect(self.x,self.y,self.taille_x,self.taille_y)
+        pygame.draw.rect(screen,self.colour,obstacle)
     
     
         
