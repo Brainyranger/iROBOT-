1232d6be6ed887cb15c3cadca8ca2fadd2687afa
Brainyranger <davidlumbwe@gmail.com>
2023-02-12 23:09:18 +0100

    fait

--- TestRoue.java
+++
@@ -1,9 +0,0 @@
-public class TestRoue{
-	public static void main(String[] args){
-		Roue r1=new Roue(120);
-		Roue r2=new Roue(120);
-		Roue r3=new Roue();
-		Roue r4=new Roue();
-		Cabine c1= new Cabine(3,"Bleu");
-	}
-}
--- iROBOT-.5/README.md
+++
@@ -1,3 +0,0 @@
-IROBOT's GROUP 
-
-lien trello : https://trello.com/b/6hB6e18V/irobot
