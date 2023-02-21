9e274937ef2a522741ee67339e0532aa48b72bb9
greggggggggggggggg <122972887+greggggggggggggggg@users.noreply.github.com>
2023-02-21 15:59:34 +0000

    Création d'une classe Simulation_pygame pour l'affichage

---
+++ projet_robot/projet_robot/Simulation_pygame.py
@@ -0,0 +1,45 @@
+import time
+import math,pygame
+from pygame.locals import *
+from  projet_robot.Controller.IA import IA,IA_avancer
+from  projet_robot.Simulation.Robot import Robot
+from  projet_robot.Simulation.Obstacle import Obstacle
+from  projet_robot.Simulation.Senseur import Senseur
+
+class Simulation_pygame:
+
+    def __init__(self,bord_map_x,bord_map_y)-> None:
+        """ Initialise les éléments de notre simulation"""
+        self.green = (0,255,0)
+        self.colour = (0,0,255)
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((bord_map_x,bord_map_y))
+        self.image_robot = pygame.image.load("images.jpg")
+
+    def event_update(self,simul):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                simul.running = False
+        self.screen.fill("White")
+        self.draw_robot(simul.robot)
+        self.draw_senseur(simul.senseur,simul.robot)
+        for i in range(0,len(simul.list_obs)):	
+            self.draw_obstacle(simul.list_obs[i][0],simul.list_obs[i][1],simul.list_obs[i][2],simul.list_obs[i][3])
+        pygame.display.flip()
+        self.clock.tick(50)
+
+        
+    def draw_robot(self,robot):
+        """ Affiche le robot """
+        rotated = pygame.transform.rotozoom(self.image_robot,math.degrees(robot.h),1)
+        recto = rotated.get_rect(center=(robot.x,robot.y))
+        self.screen.blit(rotated,recto)
+        
+    def draw_senseur(self,senseur,robot):
+        """ Affiche le senseur """
+        pygame.draw.line(self.screen,self.green,(robot.x,robot.y),((robot.x + senseur.portee * math.cos(robot.h)),(robot.y - senseur.portee * math.sin(robot.h))))
+
+    def draw_obstacle(self,x,y,taille_x,taille_y):
+        """ Affiche l'obstacle """
+        obstacle = pygame.Rect(x,y,taille_x,taille_y)
+        pygame.draw.rect(self.screen,self.colour,obstacle)
