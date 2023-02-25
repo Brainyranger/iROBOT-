8b863458a80f4a66fa20af049b7d66ae1e8c728b
greggggggggggggggg <122972887+greggggggggggggggg@users.noreply.github.com>
2023-02-25 16:22:27 +0000

    Ajout dans le main de la création de commandes pour l'IA

--- projet_robot/projet_robot/Simulation/Main_simulation.py
+++ projet_robot/projet_robot/Simulation/Main_simulation.py
@@ -6,13 +6,16 @@ pygame.init()
 pygame.display.set_caption("Ma simulation")
 bord_map_x = 500
 bord_map_y = 420
-simul = Environnement(bord_map_x,bord_map_y)
-IA_robot = IA(0.05*3800,200,simul.robot)
-while simul.running :
-        simul.update()
-        IA_robot.update(simul.dt)
-        simul.simul_pygame.event_update(simul)
-        simul.running = IA_robot.s
+Simul = Environnement(bord_map_x,bord_map_y)
+Simul_pygame = Simulation_pygame(Simul.bord_map_x,Simul.bord_map_y)
+ToutDroit = Avancer(0.03,2000,Simul.robot)
+IA = IA()
+IA.ajout_commandes(ToutDroit)
+while Simul.running :
+        IA.update(Simul.robot,Simul.dt)
+        Simul.update()
+        Simul_pygame.event_update(Simul)
+        Simul.running = IA.getstart()
 
 pygame.QUIT()
 sys.exit()
