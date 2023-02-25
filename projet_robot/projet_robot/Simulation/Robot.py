d17e796871f157c7a72402246f6c93058cb90c91
greggggggggggggggg <122972887+greggggggggggggggg@users.noreply.github.com>
2023-02-25 16:18:33 +0000

    Ajout des méthodes de l'API dans la classe Robot

--- projet_robot/projet_robot/Simulation/Robot.py
+++ projet_robot/projet_robot/Simulation/Robot.py
@@ -9,19 +9,35 @@ class Robot:
     
   
 
-    def __init__(self,x,y,h) -> None: 
-            self.x  = x
-            self.y  = y
-            self.h  = h
-            self.motor_left  = 0.05*3800 
-            self.motor_right = 0.05*3800 
+    def __init__(self,x,y,angle) -> None: 
+        """ Initialise le robot avec une position en x et en y, une orientation et une vitesse pour la roue gauche et droite"""
+        self.x  = x
+        self.y  = y
+        self.angle  = angle
+        self.motor_left  = 0 
+        self.motor_right = 0 
         
     def move(self,dt):
-        self.x += ((self.motor_left + self.motor_right)/2) * math.cos(self.h)*dt
-        self.y -= ((self.motor_left + self.motor_right)/2) * math.sin(self.h)*dt
-        self.h += (self.motor_left - self.motor_right) / (WHEEL_BASE_WIDTH + 2*WHEEL_DIAMETER)*dt
-        #a utiliser dans la classe IA
-        if self.x>=500 or self.x<= 0 or self.y>=420 or self.y<=0 :
-            self.x -= ((self.motor_left + self.motor_right)/2) * math.cos(self.h)*dt
-            self.y += ((self.motor_left + self.motor_right)/2) * math.sin(self.h)*dt
-            self.h += 30
+        """ Déplace le robot en fonction d'un temps donné """
+        self.x += ((self.motor_left + self.motor_right)/2) * math.cos(self.angle)*dt
+        self.y -= ((self.motor_left + self.motor_right)/2) * math.sin(self.angle)*dt
+        self.angle += (self.motor_left - self.motor_right) / (WHEEL_BASE_WIDTH + 2*WHEEL_DIAMETER)*dt
+
+
+    def set_motor_dps(self, motor_left, motor_right):
+        """ Fixe la vitesse d'un moteur """
+        self.motor_left = motor_left
+        self.motor_right = motor_right
+
+    
+    def servo_rotate(self, angle):
+        """ Tourne le robot a l'angle en parametre """
+        self.angle += angle
+
+    def getmovex(self,dt):
+        """ Renvoie la position en x du robot au prochain déplacement """
+        return (self.x+((self.motor_left+self.motor_right)/2)*math.cos(self.angle)*dt) 
+
+    def getmovey(self,dt):
+        """ Renvoie la position en y du robot au prochain déplacement """
+        return (self.y-((self.motor_left+self.motor_right)/2)*math.sin(self.angle)*dt)
