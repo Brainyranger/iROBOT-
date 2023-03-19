d3b50a4364fb0544e3bba843883197241db625d9
greggggggggggggggg <122972887+greggggggggggggggg@users.noreply.github.com>
2023-03-19 20:23:20 +0000

    Ajout d'une vitesse aux obstacles (il est possible de la mettre à 0 si on veut des obstacles immobiles) et d'un angle initialisé aléatoirement

--- projet_robot/projet_robot/Simulation/Obstacle.py
+++ projet_robot/projet_robot/Simulation/Obstacle.py
@@ -1,12 +1,21 @@
 from projet_robot.Controller.Toolbox_IA import Constante
+import random,math
+
 class Obstacle:
     
-    def __init__(self,x,y,taille_x,taille_y):
+    def __init__(self,x,y,taille_x,taille_y,speed):
         """ Initialise les coordoonnées, la taille et le nom de nos obstacles """
         self.x = x
         self.y = y
         self.taille_x = taille_x
         self.taille_y = taille_y
+        self.vitesse = vitesse*3800
+        self.angle = random.uniform(0,math.pi)
+
+    def move(self,dt):
+        """ Deplace l'obstacle selon x et y en fonction de sa vitesse et de son angle """"
+        self.x += self.vitesse*math.cos(self.angle)*dt
+        self.y -= self.vitesse*math.cos(self.angle)*dt
     
 
     
