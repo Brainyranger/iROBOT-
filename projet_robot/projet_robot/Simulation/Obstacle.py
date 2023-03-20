a829619faeef6cb806197c7b6c620115c4a37bfb
Brainyranger <122823004+Brainyranger@users.noreply.github.com>
2023-03-20 01:25:27 +0000

    debug obstacle pour utiliser le déplacement de l'obstacle dans notre environnement

--- projet_robot/projet_robot/Simulation/Obstacle.py
+++ projet_robot/projet_robot/Simulation/Obstacle.py
@@ -3,7 +3,7 @@ import random,math
 
 class Obstacle:
     
-    def __init__(self,x,y,taille_x,taille_y,speed):
+    def __init__(self,x,y,taille_x,taille_y,vitesse):
         """ Initialise les coordoonnées, la taille et le nom de nos obstacles """
         self.x = x
         self.y = y
@@ -12,11 +12,10 @@ class Obstacle:
         self.vitesse = vitesse*3800
         self.angle = random.uniform(0,math.pi)
 
-    def move(self,dt):
-        """ Deplace l'obstacle selon x et y en fonction de sa vitesse et de son angle """"
-        self.x += self.vitesse*math.cos(self.angle)*dt
-        self.y -= self.vitesse*math.cos(self.angle)*dt
-    
+    def move(self,list_obs,dt):
+        """ Deplace l'obstacle selon x et y en fonction de sa vitesse et de son angle """
+        list_obs[0] += list_obs[4]*math.cos(list_obs[5])*dt
+        list_obs[1] -= list_obs[4]*math.sin(list_obs[5])*dt
 
     
     
