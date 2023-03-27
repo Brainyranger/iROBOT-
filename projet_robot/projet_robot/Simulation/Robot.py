c027d43c92983ce106e7e7e47a9fda53c05b6f9f
Brainyranger <122823004+Brainyranger@users.noreply.github.com>
2023-03-27 17:55:48 +0000

    ajout des classes abstraites à utiliser dans les proxy et ajout des leds au robot

--- projet_robot/projet_robot/Simulation/Robot.py
+++ projet_robot/projet_robot/Simulation/Robot.py
@@ -13,33 +13,62 @@ class Robot:
         self.angle  = angle*math.pi/180 #conversion degré par seconde en radian par seconde
         self.motor_left  = 0 
         self.motor_right = 0 
+        self.LED_LEFT_EYE = (255,0,0)
+        self.LED_RIGHT_EYE = (0,0,255)
         
-    def move(self,dt):
-        """ Deplace le robot selon x et y et modifie son angle """
-        self.x += ((self.motor_left + self.motor_right)/2) * math.cos(self.angle)*dt
-        self.y -= ((self.motor_left + self.motor_right)/2) * math.sin(self.angle)*dt
-        self.angle += ((self.motor_left - self.motor_right) / (largeur_robot + 2*diametre_roue))*dt
 
     def set_motor_dps(self, motor_left, motor_right):
         """ Fixe la vitesse d'un moteur """
         self.motor_left = motor_left
         self.motor_right = motor_right
 
-    
-    def move_angle(self, angle):
+
+    def get_motor_position(self):
+        """
+        Lit les etats des moteurs en degre.
+        :return: couple du  degre de rotation des moteurs
+        """
+        
+      
+        return (self.motor_left, self.motor_right)
+        
+        
+    def offset_motor_encoder(self, port, offset):
+        """
+        Fixe l'offset des moteurs (en degres) (permet par exemple de reinitialiser a 0 l'etat 
+        """
+        port = offset
+            
+    def set_led(self):
+        """ alterner les deux leds """
+        tmp = self.LED_LEFT_EYE 
+        self.LED_LEFT_EYE = self.LED_RIGHT_EYE
+        self.LED_RIGHT_EYE = tmp 
+
+    def move_angle(self, angle,direction):
         """ Tourne le robot a l'angle en parametre """
-        self.angle += angle*math.pi/180
-        if self.angle > 2*math.pi:
-            self.angle -= 2*math.pi
+        pass
 
     def getAngleEnDegre(self):
         """ Renvoie l'angle dur robot en degré"""
-        return self.angle*180/math.pi
+        pass
 
     def getmovex(self,dt):
         """ Simule le déplacement du robot en x selon un temps dt """
-        return (self.x+((self.motor_left+self.motor_right)/2)*math.cos(self.angle)*dt) 
-
+        pass
     def getmovey(self,dt):
         """ Simule le déplacement du robot en y selon un temps dt """
-        return (self.y-((self.motor_left+self.motor_right)/2)*math.sin(self.angle)*dt)
+        pass
+
+    def set_led_left(self,colour):
+        """change la couleur led gauche"""
+        pass
+        
+    def set_led_right(self,colour):
+        """change la couleur led droite"""
+        pass
+        
+    def move(self,dt):
+        """ Deplace le robot selon x et y et modifie son angle """
+        pass
+
