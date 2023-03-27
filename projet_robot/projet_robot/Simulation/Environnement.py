c8ce3bf3cc0f364426eef1603f7b88fc836c875a
Brainyranger <122823004+Brainyranger@users.noreply.github.com>
2023-03-27 18:01:00 +0000

    refactorisation de detection collision, méthode trop lourd subdivisions en detection bord map, detcetion obstacle et detection collision

--- projet_robot/projet_robot/Simulation/Environnement.py
+++ projet_robot/projet_robot/Simulation/Environnement.py
@@ -3,7 +3,7 @@ import math
 import random
 from threading import Thread
 from projet_robot.Controller.IA import IA
-from projet_robot.Controller.Toolbox_IA import largeur_robot,portee_senseur
+from projet_robot.Controller.Proxy_IA import largeur_robot,portee_senseur,Proxy_simulation as proxy_simul
 from projet_robot.Simulation.Robot import Robot
 from projet_robot.Simulation.Obstacle import Obstacle
 from projet_robot.Simulation.Senseur import Senseur
@@ -24,29 +24,40 @@ class Environnement(Thread):
         self.list_obs = self.list_obs_mobiles + self.list_obs_immobiles
         
 	
-	
-    def detection_collision(self):
+def detection_collision_bord_map_robot(self):
         """détection des collisions"""
         #Détection des bords de map
         if self.robot.x >= self.bord_map_x or self.robot.x <= 0 or self.robot.y >= self.bord_map_y or self.robot.y <= 0 :
                 print("COLLISION MUR")
-                self.robot.move_angle(180)
+                proxy_simul.move_angle(self.robot,180,"gauche")
+                return True
+    
+    def detection_collision_bord_map_obstacle(self):
+        """détection des collisions"""
         for k in range(0,len(self.list_obs_mobiles)):
             if self.list_obs_mobiles[k][0] >= self.bord_map_x-self.list_obs_mobiles[k][2] or self.list_obs_mobiles[k][0] <= 0 or self.list_obs_mobiles[k][1] >= self.bord_map_y-self.list_obs_mobiles[k][3] or self.list_obs_mobiles[k][1] <= 0:
                 self.list_obs_mobiles[k][4] = - self.list_obs_mobiles[k][4]
-                print("OBSTACLE REPONDIT SUR LE MUR")
-
-        #Détection par le senseur
+                print("OBSTACLE REBONDIT SUR LE MUR")
+    
+    
+    def detection_obstacle(self):
+        """détection des obstacles"""
         self.list_obs = self.list_obs_mobiles + self.list_obs_immobiles
         for i in range(0,len(self.list_obs)):
             dist_robot_obstacle = self.senseur.get_distance(self.robot,self.list_obs[i][0],self.list_obs[i][1],self.list_obs[i][2],self.list_obs[i][3])
             if dist_robot_obstacle != False:
                 print("Le senseur a détecté un obstacle à "+str(dist_robot_obstacle)+" cm")
-            #Détection par la simulation
+                return True
+        return False
+    
+    def detection_collision(self):
+        """détection des collisions"""
+        for i in range(0,len(self.list_obs)):
             for j in range(0,largeur_robot):
                 if self.robot.x+(j-largeur_robot/2)*math.cos(self.robot.angle) >= self.list_obs[i][0] and self.robot.y+(j-largeur_robot/2)*math.sin(self.robot.angle) >= self.list_obs[i][1] and self.robot.x+(j-largeur_robot/2)*math.cos(self.robot.angle) <= (self.list_obs[i][0]+self.list_obs[i][2]) and self.robot.y+(j-largeur_robot/2)*math.sin(self.robot.angle) <= (self.list_obs[i][1]+self.list_obs[i][3]):
                     print("COLLISION")
                     return True
+            
         return False 
 
     def generer_obstacles(self,nb_obs,speed):
@@ -71,5 +82,8 @@ class Environnement(Thread):
     def update(self,dt):
         """ fais la mise à jour de notre simulation """
         self.move_obstacles(dt)
-        self.robot.move(dt)
+        proxy_simul.move(self.robot,dt)
         self.detection_collision()
+        self.detection_obstacle()
+        self.detection_collision_bord_map_robot()
+        self.detection_collision_bord_map_obstacle()
