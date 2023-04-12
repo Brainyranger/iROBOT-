d51852c5484a8771516ca9f13659a3d8cdec136a
Brainyranger <davidlumbwe@gmail.com>
2023-04-12 10:31:33 +0200

    robot debug

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ robot_mockup = Robot_Mockup()
 #initialisation de l'environnment
 simul = Environnement(bord_map_x,bord_map_y,robot,senseur)
 #initialisation de notre affichage
-simul_pygame = Simulation_pygame(simul.bord_map_x,simul.bord_map_y)
+#simul_pygame = Simulation_pygame(simul.bord_map_x,simul.bord_map_y)
 #énumération des commandes de notre IA pour le robot virtuel
 
 #commandes pour aller tout droit suivant une vitesse et une distance donnée
@@ -40,11 +40,11 @@ IA_avance_mockup = Avancer(0.03,10,robot_mockup)
 IA_tourne_droit_mockup = Tourner(0.008,-90,robot_mockup)
 IA_tourne_gauche_mockup = Tourner(0.008,90,robot_mockup)
 #commandes générique pour le mockup
-#IA = IA([IA_avance_mockup,IA_tourne_droit_mockup,IA_tourne_gauche_mockup])
+IA = IA([IA_avance_mockup,IA_tourne_droit_mockup,IA_tourne_gauche_mockup])
 #commandes pour sélectionner par indice quelle IA on veut éxécute
-#IA = IA.select_commandes(1)
+IA = IA.select_commandes(0)
 #commandes pour tracer un carré avec le mockup
-IA = IA([IA_avance_mockup,IA_tourne_gauche_mockup,IA_avance_mockup,IA_tourne_gauche_mockup,IA_avance_mockup,IA_tourne_gauche_mockup,IA_avance_mockup,IA_tourne_gauche_mockup])
+#IA = IA([IA_avance_mockup,IA_tourne_gauche_mockup,IA_avance_mockup,IA_tourne_gauche_mockup,IA_avance_mockup,IA_tourne_gauche_mockup,IA_avance_mockup,IA_tourne_gauche_mockup])
 
 
 
@@ -52,7 +52,7 @@ IA = IA([IA_avance_mockup,IA_tourne_gauche_mockup,IA_avance_mockup,IA_tourne_gau
 temps = time.time()
 #lancer les thread 
 simul.start()
-simul_pygame.start()
+#simul_pygame.start()
 IA.start()
 
 while simul.running :
@@ -60,7 +60,7 @@ while simul.running :
         temps = time.time()
         IA.update(temps_reel)
         simul.update(temps_reel)
-        simul_pygame.event_update(simul)
+        #simul_pygame.event_update(simul)
         simul.running = IA.getStatus()
 
 
