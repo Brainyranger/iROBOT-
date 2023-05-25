dde0051eda4285041fb7c55532da2d2f68e1182f
Brainyranger <davidlumbwe@gmail.com>
2023-05-25 20:01:41 +0200

    rajout de l'emplacement où il faut stocker

--- get_image.py
+++ get_image.py
@@ -1,3 +1,6 @@
 from main import robot_reel
+from PIL import Image
+from projet_robot.Controller.Constante import chemin_images_reel
 
-robot_reel.get_image()
+image = robot_reel.get_image()
+image.save(chemin_images_reel+"/image_n°0.jpeg")
